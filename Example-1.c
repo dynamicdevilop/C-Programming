db9b511f974a93b69a30b0a21778061fd33be503
Dynamic DevilOP <120082221+dynamicdevilop@users.noreply.github.com>
2023-01-06 13:19:46 +0530

    Create Example-1.c
    Check whether the number is even or odd.

---
+++ Example-1.c
@@ -0,0 +1,18 @@
+// 'C' Program to check whether a number is odd or even
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    printf("Enter a Number : ");
+    scanf("%d",&number);
+    
+    if(number % 2 == 0)
+    {
+        printf("Number = %d , is Even.");
+    } else
+        {
+            printf("Number = %d , is Odd.");
+        }
+    return 0;
+}
